c071db5158a73e310448af257bc9fd0e1e22c801
VVSK <vvskg1123@gmail.com>
2021-06-14 13:55:22 -0700

    backup

---
+++ force-app/main/default/aura/StudentDetails/StudentDetailsController.js
@@ -0,0 +1,9 @@
+({
+	handleStudentDetails : function(component, event, helper) {
+		helper.handleStudentetails(component, event)
+	},
+    
+    handleClubDetails : function(component, event, helper) {
+        helper.handleClubDetails(component)
+    }
+})
