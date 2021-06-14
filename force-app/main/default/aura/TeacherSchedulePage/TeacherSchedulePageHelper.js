e780c8446487ebaa4ca2776f4f5573649074b970
Natalie Clark <moonshine.machiavelli@gmail.com>
2021-06-14 03:20:13 -0400

    updated Teacher Schedule function, modified time field, added test class

--- force-app/main/default/aura/TeacherSchedulePage/TeacherSchedulePageHelper.js
+++ force-app/main/default/aura/TeacherSchedulePage/TeacherSchedulePageHelper.js
@@ -1,25 +1,26 @@
 ({
-    //initializes the list of teachers in the datatable at the top of the page
-    doInit : function(component) {
-        let teachers = component.get("c.getTeachers")
+    //initializes the list of teachers in the datatable at the top of the page, and sets the class list for later reference
+    doInit : function(component, event) {
+        var teachers = component.get("c.getTeachers")
 
-        teachers.setCallback(this, $A.getCallback(function(response){
+        teachers.setCallback(this, function(response){
             if(response.getState() === "SUCCESS") {
                 component.set("v.teachers", response.getReturnValue())
             } else if (state === "ERROR") {
                 var errors = response.getError();
                 console.error(errors);
             }
-        }))
+        })
+
         $A.enqueueAction(teachers)
     },
 
     //used to set the weekly schedule recordViewForm based on the selection from the lightning:select 
-    handleSelect: function(component, event) {
-        let classList = component.get("c.getWeeklyMeetingTimes")
+    teacherSelect: function(component, event) {
+        component.set("v.showAndTell", true) //this line enables the aura:if functionality on first selection of a teacher
+        component.set("v.currentTeacher", event.getParam("selectedRows")[0]) //this line sets the current teacher to display above the schedule
 
-        component.set("v.showAndTell", true) //this line enables the aura:if functionality
-        component.set("v.currentTeacher", event.getParam("selectedRows")[0]) //this line sets the currentTeacher for reference in header in the aura:if
+        var classList = component.get("c.getWeeklyMeetingTimes")
 
         classList.setParams({tea: event.getParam("selectedRows")[0]}) 
 
@@ -35,9 +36,27 @@
         $A.enqueueAction(classList)
     },
 
+    //sets the list of classes to display for selection, that don't repeat due to multiple class times
+    classRosterSet: function(component, event) {
+        var roster = component.get("c.getRoster")
+
+        roster.setParams({tea: event.getParam("selectedRows")[0]}) 
+
+        roster.setCallback(this, function(response){
+            if(response.getState() === "SUCCESS") {
+                component.set("v.classRoster", response.getReturnValue())
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                console.error(errors);
+            }
+        })
+
+        $A.enqueueAction(roster)
+    },
+
     //used to change the daily schedule based on the select drop picklist containing days of the week
     changeDailyList: function(component, event) {
-        let dailyList = component.get("c.getTodayMeetingTimes") 
+        var dailyList = component.get("c.getTodayMeetingTimes") 
 
         dailyList.setParams({tea: component.find('teacherTable').getSelectedRows()[0],
                             day: component.find('selectDay').get("v.value")})
@@ -54,4 +73,23 @@
         $A.enqueueAction(dailyList)
     },
 
+    //sets current class selected, and updates list of students in class
+    rosterSelect: function(component, event) {
+        component.set("v.currentClass", event.getParam("selectedRows")[0]) //this line sets the current class to display
+
+        var classes = component.get("c.getStudents")
+
+        classes.setParams({cla: event.getParam("selectedRows")[0]}) 
+
+        classes.setCallback(this, function(response){
+            if(response.getState() === "SUCCESS") {
+                component.set("v.students", response.getReturnValue())
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                console.error(errors);
+            }
+        })
+
+        $A.enqueueAction(classes)
+    },
 })
